52de98dcbe3c351b769439a049db9aaf7b28550a
Jonghee Lee <aiden@msuefind.com>
2018-04-22 22:57:29 -0700

    set provider

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
-const App = () => {
-  return <View />;
-};
+const App = () => (
+    <Provider store={createStore(reducers)}>
+      <View />
+    </Provider>
+  );
 
 export default App;
